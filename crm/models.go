78b090b2fc3feed00e6c5e51ff768248319ef738
Dennis Kleber <dennis_kleber@mailbox.org>
2020-09-24 17:20:32 +0200

    new crm models

--- crm/models.go
+++ crm/models.go
@@ -5,6 +5,15 @@ type (
 		CampaignId int    `json:"campaign_id" validate:"required"`
 		DropsId    string `json:"drops_id" validate:"required"`
 		Activity   string `json:"activity" validate:"required"`
+		Country    string `json:"country,omitempty"`
 		Created    int64  `json:"created"`
+	}
+	CrmEmail struct {
+		Email string `json:"email"`
+		Link  string `json:"link"`
+	}
+	CrmEmailBody struct {
+		CrmData CrmData  `json:"crm_data"`
+		Mail    CrmEmail `json:"mail"`
 	}
 )
